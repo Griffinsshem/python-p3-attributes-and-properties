4609a47c674d40a8bd3af80726ff5032f92998f2
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-08-10 07:39:05 +0300

    Fix person and dog

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = ["Beagle", "Poodle", "Labrador", "German Shepherd", "Bulldog", "Pug"]
+
+    def __init__(self, name="Fido", breed="Beagle"):
+        if not isinstance(name, str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+        if breed not in Dog.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
